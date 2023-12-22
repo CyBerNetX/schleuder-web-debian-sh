f60473848c0350d1f08711e9a41e71c96ad8e2f5
Jean-Baptiste Masurel <cybernetx@cybermazout.net>
2023-12-22 23:06:12 +0100

    gem install bundler:2.5.3 source

--- install.sh
+++ install.sh
@@ -271,7 +271,7 @@ sleep 5
 echo ":ipv4_fallback_enabled: true" | $SUDO tee -a ~/.gemrc
 
 #gem install bundler -v 2.4.22
-gem install bundler:2.5.3
+gem install bundler:2.5.3  --source http://rubygems.org
 check_command
 
 echo -e "${yellow} [==============================] ${NORMAL}"
